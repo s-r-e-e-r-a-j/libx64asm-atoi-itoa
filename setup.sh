30fce791d63ff97ea5531048d29581aa7250d480
Sreeraj S Kurup <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2026-01-26 21:46:59 +0530

    Update setup.sh to support multiple Linux distros

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 if ! command -v nasm &> /dev/null; then
-    sudo apt update
-    sudo apt install -y nasm
+    if command -v apt &> /dev/null; then
+        sudo apt update
+        sudo apt install -y nasm
+    elif command -v dnf &> /dev/null; then
+        sudo dnf install -y nasm
+    elif command -v yum &> /dev/null; then
+        sudo yum install -y nasm
+    elif command -v pacman &> /dev/null; then
+        sudo pacman -Sy --noconfirm nasm
+    else
+        echo "No supported package manager found. Please install NASM manually."
+        exit 1
+    fi
 fi
 
 mkdir -p build
